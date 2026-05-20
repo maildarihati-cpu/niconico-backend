d57c1f366e0e303250211c0f4a0212b3724d5eeb
Pro <pro@MacBook-Pro-Pro.local>
2026-05-20 13:11:04 +0700

    xendit 7

--- src/modules/xendit/index.ts
+++ src/modules/xendit/index.ts
@@ -1,5 +1,7 @@
+import { ModuleProvider, Modules } from "@medusajs/framework/utils"
 import XenditPaymentProvider from "./service"
 
-export default {
+// 🌟 Kita pakaikan seragam ModuleProvider agar Medusa mau mendaftarkannya!
+export default ModuleProvider(Modules.PAYMENT, {
   services: [XenditPaymentProvider],
-}
+})
